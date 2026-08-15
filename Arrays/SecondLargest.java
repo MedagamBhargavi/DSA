17cb9736f9733b4994c54a8bc2631e6b6df63ca8
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-15 17:01:44 +0530

    Create SecondLargest.java

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,27 @@
+//Find the largest element
+public class SecondLargest {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+         for(int i=0;i<n;i++){
+              arr[i] = sc.nextInt();
+        }
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+              if(arr[i]>first){
+                second = first;
+                first = arr[i];
+              }else if(arr[i]>second && arr[i]!=first){
+                second = arr[i];
+              }
+        }
+        if(second == Integer.MIN_VALUE){
+        System.out.println("NO second largest element");
+        }else{
+            System.out.println(second);
+        }
+        
+    }
+}
