f26573ba9a6c906e38074faf85104c24ba71dd22
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-19 11:31:54 +0530

    Update ReverseString.java

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -4,7 +4,7 @@ class ReverseString{
     public static void main(String args[]){ 
         Scanner sc = new Scanner(System.in); 
         String s = sc.next();                  // s = Ramu
-        String rev =" ";
+        String rev ="";
         for(int i=s.length()-1;i>=0;i--){
             rev += s.charAt(i);
         } 
