3a09e09cc4003a51e4c4718bd134dc026bcda837
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-19 14:28:05 +0530

    Create UpperAndLowerCaseConverstion.java

---
+++ Strings/UpperAndLowerCaseConverstion.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+public class UpperAndLowerCaseConverstion{ 
+   
+    public static void main(String args[]){ 
+        Scanner sc = new Scanner(System.in);        
+        String s = sc.next();                        // s = RamYA
+        System.out.println("UpperCase: "+s.toUpperCase());         // s = RAMYA
+        System.out.println("LowerCase: "+s.toLowerCase());         // s = ramya
+        }
+
+    }
+
