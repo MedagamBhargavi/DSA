3808d597efb2625ed1cbe181a84066b131c4d5ec
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-22 15:00:18 +0530

    Create CheckPalindromeIgnoringSpaces.java

---
+++ Strings/CheckPalindromeIgnoringSpaces.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class CheckPalindromeIgnoringSpaces{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();                              // mad am
+        char arr[] = s.toCharArray();
+        int left = 0;
+        int right = s.length()-1;
+        boolean found = true;
+        while(left<right){
+            if(arr[left]==' '){
+                left++;
+                continue;
+            }
+            if(arr[right]==' '){
+                right--;
+                continue;
+            }
+
+            if(arr[left] != arr[right]){
+               found = false;
+            }
+            left++;
+            right--;
+        }
+        if(found){
+            System.out.println(s+" is Palindrome");         // mad am is Palindrome
+        }else{
+            System.out.println(s+" is not Palindrome");
+        }     
+    }
+
+}
+  
