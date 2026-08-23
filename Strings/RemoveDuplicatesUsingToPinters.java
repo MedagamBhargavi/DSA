469288d12f82aa31bf36e398028b99a9d51d9e1f
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-23 07:01:23 +0530

    Create RemoveDuplicatesUsingToPinters.java

---
+++ Strings/RemoveDuplicatesUsingToPinters.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class RemoveDuplicatesUsingToPointers{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();            //hello
+        char[] arr = s.toCharArray();
+        int slow = 0;
+        for(int fast = 1; fast<s.length();fast++){
+           if(arr[fast]!=arr[slow]){
+            slow++;
+            arr[slow] = arr[fast];
+           }
+        }
+        for(int i=0;i<=slow;i++){
+            System.out.print(arr[i]);           //helo
+        }
+    }
+
+}
+  
