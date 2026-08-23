f4b729053e91de71323ffec2e7350e023d02a5e6
Medagam Bhargavi <bhargavimedagam7@gmail.com>
2026-08-23 07:02:07 +0530

    Update and rename RemoveDuplicatesUsingToPinters.java to RemoveDuplicatesUsingTwoPinters.java

--- Strings/RemoveDuplicatesUsingTwoPinters.java
+++ Strings/RemoveDuplicatesUsingTwoPinters.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class RemoveDuplicatesUsingToPointers{
+public class RemoveDuplicatesUsingTwoPointers{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();            //hello
